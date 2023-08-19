c9165acc600ad42961310d569e000df9374cbbbd
jorge.lucas <jorge.lucas.18@hotmail.com>
2023-08-19 17:13:17 -0300

    atualizando projeto

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,7 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-import './commandsgw'
+//import './commandsgw'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
